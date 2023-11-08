5826d555426dd23568e6e9aa98b41d8343a89fa0
webtech <purposeresearch923@gmail.com>
2023-11-08 16:13:33 +0530

    first commit

--- src/Pages/Franchise/SuccessStories.js
+++ src/Pages/Franchise/SuccessStories.js
@@ -3,6 +3,8 @@ import ankita from '../../Images/Ankita.png'
 import manjeet from '../../Images/manjeet.png'
 import diljeet from '../../Images/diljeet.png'
 import akash from '../../Images/akash.png'
+import upperQuote from '../../Images/testimonial-bg-1.png'
+import lowerQuote from '../../Images/testimonial-bg-2.png'
 
 function SuccessStories() {
     return (
@@ -14,48 +16,56 @@ function SuccessStories() {
                         <h3>Stories</h3>
                     </div>
                 </div>
-                <div className="row testimonial-main align-items-center">
-                    <div className="col-lg-2 col-md-3">
-                        <img src={ankita} alt="" className='img-fluid'/>
-                    </div>
-                    <div className="col-lg-7 col-md-9 testimonial">
-                        <p>Kudos to Apna Chai Wala and Founder Rupesh! Your dedication and hard work have paid off, and it's evident in the success of your franchise. Your commitment to delivering top-notch products and services is exemplary, and we are proud to have you as a part of our franchise family. Keep up the fantastic work! </p>
+                <div className=" testimonial-block py-5">
+                    <div className="row testimonial-main align-items-center">
+                        <div className="col-lg-2 col-md-3">
+                            <img src={ankita} alt="" className='img-fluid' />
+                        </div>
+                        <div className="col-lg-7 col-md-9 testimonial">
+                            <p>Kudos to Apna Chai Wala and Founder Rupesh! Your dedication and hard work have paid off, and it's evident in the success of your franchise. Your commitment to delivering top-notch products and services is exemplary, and we are proud to have you as a part of our franchise family. Keep up the fantastic work! </p>
 
-                        <h2>ANKITA</h2>
-                        <h3>Banga</h3>
+                            <h2>ANKITA</h2>
+                            <h3>Banga</h3>
+                        </div>
                     </div>
-                </div>
-                <div className="row testimonial-main align-items-center justify-content-end text-end">
-                    <div className="col-lg-7 col-md-9 testimonial order-1 order-md-0">
-                        <p>Apna Chai Wala Cafe franchise has been a game-changer for my business. The brand's unique blend of flavors, cozy ambiance, and exceptional customer service have attracted a loyal following. It's a delightful experience that keeps customers coming back for more. </p>
+                    <div className="row testimonial-main align-items-center justify-content-end text-end">
+                        <div className="col-lg-7 col-md-9 testimonial order-1 order-md-0">
+                            <p>Apna Chai Wala Cafe franchise has been a game-changer for my business. The brand's unique blend of flavors, cozy ambiance, and exceptional customer service have attracted a loyal following. It's a delightful experience that keeps customers coming back for more. </p>
 
-                        <h2>MANJEET</h2>
-                        <h3>Hoshiarpur</h3>
+                            <h2>MANJEET</h2>
+                            <h3>Hoshiarpur</h3>
+                        </div>
+                        <div className="col-lg-2 col-md-3">
+                            <img src={manjeet} alt="" className='img-fluid' />
+                        </div>
                     </div>
-                    <div className="col-lg-2 col-md-3">
-                        <img src={manjeet} alt="" className='img-fluid'/>
-                    </div>
-                </div>
-                <div className="row testimonial-main align-items-center">
-                    <div className="col-lg-2 col-md-3">
-                        <img src={diljeet} alt="" className='img-fluid'/>
-                    </div>
-                    <div className="col-lg-7 col-md-9 testimonial">
-                        <p>Choosing Apna Chai Wala Cafe franchise was the best decision I made. The franchise support and training provided were exceptional. The menu selection, quality ingredients, and inviting atmosphere have made our cafe a favorite destination in town.</p>
+                    <div className="row testimonial-main align-items-center">
+                        <div className="col-lg-2 col-md-3">
+                            <img src={diljeet} alt="" className='img-fluid' />
+                        </div>
+                        <div className="col-lg-7 col-md-9 testimonial">
+                            <p>Choosing Apna Chai Wala Cafe franchise was the best decision I made. The franchise support and training provided were exceptional. The menu selection, quality ingredients, and inviting atmosphere have made our cafe a favorite destination in town.</p>
 
-                        <h2>diljeet</h2>
-                        <h3>Nawanshahr</h3>
+                            <h2>diljeet</h2>
+                            <h3>Nawanshahr</h3>
+                        </div>
                     </div>
-                </div>
-                <div className="row testimonial-main justify-content-end text-end align-items-center">
-                    <div className="col-lg-7 col-md-9 testimonial order-1 order-md-0">
-                        <p>Apna Chai Wala Cafe franchise has exceeded my expectations. From the first sip of their aromatic chai to the delicious assortment of snacks, every visit is a treat. The brand's commitment to excellence and creating a welcoming environment truly sets them apart.</p>
+                    <div className="row testimonial-main justify-content-end text-end align-items-center">
+                        <div className="col-lg-7 col-md-9 testimonial order-1 order-md-0">
+                            <p>Apna Chai Wala Cafe franchise has exceeded my expectations. From the first sip of their aromatic chai to the delicious assortment of snacks, every visit is a treat. The brand's commitment to excellence and creating a welcoming environment truly sets them apart.</p>
 
-                        <h2>AKASH</h2>
-                        <h3>Bhubaneswar</h3>
+                            <h2>AKASH</h2>
+                            <h3>Bhubaneswar</h3>
+                        </div>
+                        <div className="col-lg-2 col-md-3">
+                            <img src={akash} alt="" className='img-fluid' />
+                        </div>
                     </div>
-                    <div className="col-lg-2 col-md-3">
-                        <img src={akash} alt="" className='img-fluid'/>
+                    <div className="upper-quote">
+                        <img src={upperQuote} alt="" />
+                    </div>
+                    <div className="lower-quote">
+                        <img src={lowerQuote} alt="" />
                     </div>
                 </div>
             </div>
