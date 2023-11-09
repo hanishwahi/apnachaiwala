ee25820f6ffb3c737860da3a6e3efffbb1421e96
webtech <purposeresearch923@gmail.com>
2023-11-09 18:28:19 +0530

    first commit

--- src/Pages/Franchise/FranchiseInput.js
+++ src/Pages/Franchise/FranchiseInput.js
@@ -15,12 +15,29 @@ function FranchiseInput() {
 
         const userDetails= {name, phone, email, address, budget, profession}
 
-        if (!name || !phone || !email || !address || !budget || !profession){
-            setError(true)
+        const validationErrors= {}
+
+        if (!name){
+            validationErrors.name="name is required"
         }
-        else {
-            setError(false)
+        if (!phone){
+            validationErrors.phone="phone is required"
         }
+        if (!email){
+            validationErrors.email="email is required"
+        }
+        if (!address){
+            validationErrors.address="address is required"
+        }
+        if (!budget){
+            validationErrors.budget="budget is required"
+        }
+        if (!profession){
+            validationErrors.profession="profession is required"
+        }
+         
+            setError(validationErrors)
+        
          
             console.log(userDetails)
     }
@@ -33,33 +50,33 @@ function FranchiseInput() {
                         <div className="row">
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Name</label> <br />
-                                <input onChange={(e)=>{setName(e.target.value); setError(false)}} type="text" placeholder='Enter Your Name' name='name' autoComplete='off'/>  
-                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
+                                <input onChange={(e)=>{setName(e.target.value)}} type="text" placeholder='Enter Your Name' name='name' autoComplete='off'/>  
+                                {err.name ? <><span style={{color:"red"}}>{err.name}</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Phone</span> Number</label> <br />
-                                <input onChange={(e)=>{setPhone(e.target.value); setError(false)}}   type="number" placeholder='Enter Phone Number' name='phone' autoComplete='off'/>
-                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
+                                <input onChange={(e)=>{setPhone(e.target.value)}}   type="number" placeholder='Enter Phone Number' name='phone' autoComplete='off'/>
+                                {err.phone ? <><span style={{color:"red"}}>{err.phone}</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Email</span> Address</label> <br />
-                                <input onChange={(e)=>{setEmail(e.target.value); setError(false)}}   type="email" placeholder='Enter Email Address' autoComplete='off'/>
-                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
+                                <input onChange={(e)=>{setEmail(e.target.value)}}   type="email" placeholder='Enter Email Address' autoComplete='off'/>
+                                {err.email ? <><span style={{color:"red"}}>{err.email}</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Address</label> <br />
-                                <input onChange={(e)=>{setAddress(e.target.value); setError(false)}}   type="text" placeholder='Enter Your Address' autoComplete='off'/>
-                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
+                                <input onChange={(e)=>{setAddress(e.target.value)}} name='address'  type="text" placeholder='Enter Your Address' autoComplete='off'/>
+                                {err.address ? <><span style={{color:"red"}}>{err.address}</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Budget</label> <br />
-                                <input onChange={(e)=>{setBudget(e.target.value); setError(false)}}    type="text" placeholder='Enter Your Budget' autoComplete='off'/>
-                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
+                                <input onChange={(e)=>{setBudget(e.target.value)}}  name='budget'  type="text" placeholder='Enter Your Budget' autoComplete='off'/>
+                                {err.budget ? <><span style={{color:"red"}}>{err.budget}</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Profession</label> <br />
-                                <input onChange={(e)=>{setProfession(e.target.value); setError(false)}}   type="text" placeholder='Enter Your Profession' autoComplete='off'/>
-                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
+                                <input onChange={(e)=>{setProfession(e.target.value)}} name='profession'  type="text" placeholder='Enter Your Profession' autoComplete='off'/>
+                                {err.profession ? <><span style={{color:"red"}}>{err.profession}</span></>: null}
                             </div>
                             <div className="col-lg-12">
                                 <div className="row align-items-center justify-content-center form-inner-radio">
