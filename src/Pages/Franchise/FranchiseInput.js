1c3576a9e1b21fc9f17578f27102bb458f4a86d9
webtech <purposeresearch923@gmail.com>
2023-11-09 18:16:38 +0530

    first commit

--- src/Pages/Franchise/FranchiseInput.js
+++ src/Pages/Franchise/FranchiseInput.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function FranchiseInput() {
+   const [name, setName] = useState("")
+   const [phone, setPhone] = useState("")
+   const [email, setEmail] = useState("")
+   const [address, setAddress] = useState("")
+   const [budget, setBudget] = useState("")
+   const [profession, setProfession] = useState("")
+   const [err, setError] = useState(false)
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const userDetails= {name, phone, email, address, budget, profession}
+
+        if (!name || !phone || !email || !address || !budget || !profession){
+            setError(true)
+        }
+        else {
+            setError(false)
+        }
+         
+            console.log(userDetails)
+    }
+
     return (
         <>
             <div className="container-fluid franchise-form-main">
@@ -9,27 +33,33 @@ function FranchiseInput() {
                         <div className="row">
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Name</label> <br />
-                                <input type="text" placeholder='Enter Your Name' />
+                                <input onChange={(e)=>{setName(e.target.value); setError(false)}} type="text" placeholder='Enter Your Name' name='name' autoComplete='off'/>  
+                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Phone</span> Number</label> <br />
-                                <input type="number" placeholder='Enter Phone Number' />
+                                <input onChange={(e)=>{setPhone(e.target.value); setError(false)}}   type="number" placeholder='Enter Phone Number' name='phone' autoComplete='off'/>
+                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Email</span> Address</label> <br />
-                                <input type="email" placeholder='Enter Email Address' />
+                                <input onChange={(e)=>{setEmail(e.target.value); setError(false)}}   type="email" placeholder='Enter Email Address' autoComplete='off'/>
+                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Address</label> <br />
-                                <input type="text" placeholder='Enter Your Address' />
+                                <input onChange={(e)=>{setAddress(e.target.value); setError(false)}}   type="text" placeholder='Enter Your Address' autoComplete='off'/>
+                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Budget</label> <br />
-                                <input type="text" placeholder='Enter Your Budget' />
+                                <input onChange={(e)=>{setBudget(e.target.value); setError(false)}}    type="text" placeholder='Enter Your Budget' autoComplete='off'/>
+                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
                             </div>
                             <div className="col-lg-4 col-sm-6 form-inner">
                                 <label htmlFor=""><span>Your</span> Profession</label> <br />
-                                <input type="text" placeholder='Enter Your Profession' />
+                                <input onChange={(e)=>{setProfession(e.target.value); setError(false)}}   type="text" placeholder='Enter Your Profession' autoComplete='off'/>
+                                {err ? <><span style={{color:"red"}}>fill the details</span></>: null}
                             </div>
                             <div className="col-lg-12">
                                 <div className="row align-items-center justify-content-center form-inner-radio">
@@ -39,16 +69,19 @@ function FranchiseInput() {
                                     <div className='col-md-6 text-center d-flex justify-content-center justify-content-md-start'>
                                         <div className="form-radio-bg ">
                                             <div class="p-2">
-                                                <input class="form-check-input mx-2" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                                                <input class="form-check-input mx-2" type="radio" name="inlineRadioOptions" id="inlineRadio1"  />                                            
                                                 <label class="form-check-label" for="inlineRadio1">Yes</label>
                                             </div>
                                             <div class="p-2">
-                                                <input class="form-check-input mx-2" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                                                <input class="form-check-input mx-2" type="radio" name="inlineRadioOptions" id="inlineRadio2"  />                                            
                                                 <label class="form-check-label" for="inlineRadio2">No</label>
                                             </div>
                                         </div>
 
 
+                                    </div>
+                                    <div className="col">
+                                        <button onClick={handleSubmit}>Submit</button>
                                     </div>
                                 </div>
 
